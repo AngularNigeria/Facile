8fb3bff33c164c2eafd9f85bbbd27166fc10be89
Chibuike Emmauel Osita <mozart.osita@gmail.com>
2017-07-28 14:56:34 +0100

    More clean up

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ import mongoose from 'mongoose';
 mongoose.Promise = require('bluebird');
 import config from '../config/environment';
 import http from 'http';
-// import seedDatabaseIfNeeded from './config/seed';
 
 // Connect to MongoDB
 mongoose.connect(config.mongo.uri, config.mongo.options);
